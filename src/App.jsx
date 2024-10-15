0fa8a294fcad7136ba7ce02300f84b880b1ea139
ArtemBlyshchyk <artem.bl31@gmail.com>
2024-10-15 15:57:46 +0300

    Created components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import ContactForm from "./components/ContactForm/ContactForm.jsx";
+import SearchBox from "./components/SearchBox/SearchBox.jsx";
+import ContactList from "./components/ContactList/ContactList.jsx";
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
 }
 
 export default App;
